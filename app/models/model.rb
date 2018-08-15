4542eafb7a8bf45a737d7c2e0acc71341c24729d
mimigood <mbgss07@gmail.com>
2018-08-15 13:53:48 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,18 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  # Replace with Dog class
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed
+    @age = age 
+    DOGS << self 
+  
 end
+
+def self.assert_select_all
+  DOGS
+end 
+end 
+
+
